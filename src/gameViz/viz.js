29b6d37a98635db20ee04431c00c59887e1e65a9
Vinesh Kannan <vinesh@omnipointment.com>
2017-10-01 04:36:33 -0700

    Add visualizer

---
+++ src/gameViz/viz.js
@@ -0,0 +1,218 @@
+const { evaluate } = require('../lib/evaluate');
+
+module.exports = function GameViz(params) {
+
+	const GRID_SCALE = 10;
+	const DT = 0.1;
+	const T_STEP = 50;
+	
+	const stage = params.stage;
+	
+	let finalCallback;
+
+	function initAnimations(params) {
+		return new Promise((resolveAll, rejectAll) => {
+			let objects = params.objects;
+			let scaler = params.scaler;
+			if (objects.length === 0) {
+				resolveAll(true);
+			}
+			let animations = [];
+			objects.forEach((obj) => {
+				let promise = new Promise((resolve, reject) => {
+					let x = scaler.getX(obj.data, params.t);
+					let y = scaler.getY(obj.data, params.t);
+					let attr = {
+						x: x,
+						y: y
+					}
+					obj.g.animate(attr, params.step);
+				});
+				animations.push(promise);
+			});
+			//Promise.all(animations).then((done) => {
+			setTimeout(() => {
+				params.t += params.dt;
+				if (params.t < params.max) {
+					initAnimations(params).then(resolveAll).catch(rejectAll);
+				}
+			}, params.step);
+			//});
+		});
+	}
+
+	function scaleValue(val, worldBounds, viewBounds) {
+		let worldVal = val - worldBounds[0];
+		let ratio = worldVal / (worldBounds[1] - worldBounds[0]);
+		let viewVal = (ratio * (viewBounds[1] - viewBounds[0])) + viewBounds[0];
+		return viewVal;
+	}
+
+	function scaleSize(val) {
+		return val * GRID_SCALE;
+	}
+
+	function Scaler(params) {
+		let wb = params.world;
+		let vb = params.view;
+		return {
+			getX (data, t) {
+				let val;
+				if (typeof data.x === 'string') {
+					val = evaluate(data.x, {t});
+				} else {
+					val = data.x;
+				}
+				let n = scaleValue(val, wb.x, vb.x);
+				return n;
+			},
+			getY (data, t) {
+				let val;
+				if (typeof data.y === 'string') {
+					val = evaluate(data.y, {t});
+				} else {
+					val = data.y;
+				}
+				if (data.height) {
+					val -= data.height;
+				}
+				let n = scaleValue(val, wb.y, vb.y);
+				if (data.height) {
+					//console.log(data.height)
+					//let hS = scaleSize(data.height);
+					//console.log(n, hS)
+					//n -= scaleSize(data.height);
+				}
+				return n;
+			}
+		}
+	}
+
+	function drawGridLines(s, wb, vb, opt) {
+		for (let ix = wb.x[0]; ix <= wb.x[1]; ix += opt.step) {
+			let line = s.line().attr({
+				x1: scaleValue(ix, wb.x, vb.x),
+				y1: scaleValue(wb.y[0], wb.y, vb.y),
+				x2: scaleValue(ix, wb.x, vb.x),
+				y2: scaleValue(wb.y[1], wb.y, vb.y),
+				stroke: opt.stroke,
+				strokeWidth: ix === 0 ? 4 : 0.25
+			});
+		}
+		for (let iy = wb.y[0]; iy <= wb.y[1]; iy += opt.step) {
+			let line = s.line().attr({
+				x1: scaleValue(wb.x[0], wb.x, vb.x),
+				y1: scaleValue(iy, wb.y, vb.y),
+				x2: scaleValue(wb.x[1], wb.x, vb.x),
+				y2: scaleValue(iy, wb.y, vb.y),
+				stroke: opt.stroke,
+				strokeWidth: iy === 0 ? 2 : 0.25
+			});
+		}
+	}
+	
+	return {
+
+		init () {
+
+			let s = Snap(params.output);
+
+			let svgOut = document.querySelector('#svg');
+			svgOut.setAttribute('width', GRID_SCALE * (stage.domain[1] - stage.domain[0]));
+			svgOut.setAttribute('height', GRID_SCALE * (stage.range[1] - stage.range[0]));
+
+			let WIDTH = s.node.width.baseVal.value;
+			let HEIGHT = s.node.height.baseVal.value;
+
+			const WORLD = {
+				x: stage.domain,
+				y: stage.range
+			};
+
+			const VIEW = {
+				x: [0, WIDTH],
+				y: [HEIGHT, 0]
+			};
+
+			let background = s.rect(0, 0, WIDTH, HEIGHT);
+			background.attr({
+				fill: stage.background
+			});
+
+			drawGridLines(s, WORLD, VIEW, {
+				stroke: '#54BAD1',
+				step: 5
+			});
+
+			let objects = [];
+			let scaler = Scaler({
+				world: WORLD,
+				view: VIEW
+			});
+
+			let thingies = [];
+			stage.obstacles.forEach((d) => {
+				thingies.push(d);
+			});
+			stage.coins.forEach((d) => {
+				d.type = 'coin';
+				thingies.push(d);
+			});
+
+			thingies.forEach((data) => {
+				let xS = scaler.getX(data, 0);
+				let yS = scaler.getY(data, 0);
+				let attr = {
+					x: xS,
+					y: yS
+				}
+				let g;
+				if (data.type === 'coin') {
+					let rS = scaleSize(1);
+					g = s.circle(xS, yS, rS).attr(attr);
+				} else {
+					let wS = scaleSize(data.width);
+					let hS = scaleSize(data.height);
+					attr.width = wS;
+					attr.height = hS;
+					g = s.rect(xS, yS, wS, hS).attr(attr);
+				}
+				//let g = s.image('cloud.png', 0, 0, 0, 0).attr(attr);
+				
+				objects.push({
+					g: g,
+					data: data,
+					isObstacle: true
+				});
+			});
+
+			const T_MAX = 0.2//WORLD.x[1] - WORLD.x[0];
+
+			initAnimations({
+				objects: objects,
+				scaler: scaler,
+				t: 0,
+				dt: DT,
+				step: T_STEP,
+				max: T_MAX
+			}).then((done) => {
+				if (finalCallback) {
+					finalCallback({
+						actions: []
+					});
+				}
+			});
+
+		},
+
+		changePlayerFormula (formulaString) {
+
+		},
+
+		onEnd (callback) {
+			finalCallback = callback;
+		}
+
+	}
+
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,284 @@
 const { evaluate } = require('./lib/evaluate')
 const MathField = require('./mathField/mathField')
+const GameViz = require('./gameViz/viz')
+
+const STAGE = {
+	title: 'My First Level',
+	background: 'white',
+	domain: [0, 100], 
+	range: [-10, 30], 
+	obstacles: [
+		{
+			sprite : 'cloud',
+			x : 25,
+			y : 20,
+			width : 5,
+			height : 3
+
+		},
+		{
+			sprite : 'building1', //short building
+			x : 24,
+			y : -10,
+			width : 15,
+			height : 45
+		},
+
+		{
+			sprite: 'building2', //dome
+			x: 39,
+			y: -10,
+			width: 40,
+			height: 60
+			
+		},
+		{
+			sprite : 'building3', //skinny medium 
+			x : 76,
+			y : -10,
+			width : 10,
+			height: 40
+		},
+
+		{
+			sprite : 'building4', // skinnt tall
+			x : 86,
+			y : -10,
+			width : 15,
+			height : 60
+		},
+		
+		{
+			sprite : 'balloon',
+			x : 60,
+			y : 't',
+			width: 5,
+			height: 5
+		},
+
+		{
+			sprite : 'balloon',
+			x : 62,
+			y: 't/2',
+			width: 5,
+			height: 5
+		},
+
+		{
+			sprite : 'balloon',
+			x : 64,
+			y : 't/3',
+			width: 5,
+			height: 5
+		},
+
+		{
+			sprite: 'bird',
+			x: 50,
+			y: '10 * sin(t)',
+			width: 5,
+			height: 5
+		},
+		{
+			sprite: 'bird',
+			x: '100 - t',
+			y: '10 * sin(100 - t)',
+			width: 5,
+			height: 5
+		}
+
+		
+	],
+	coins: [
+		
+
+		{
+			value : 1,
+			x : 12,
+			y : 't - 10'
+
+		},
+
+		{
+			value : 1,
+			x : 14,
+			y : 't - 10'
+
+		},
+
+		{
+			value : 1,
+			x : 16,
+			y : 't - 10'
+
+		},
+
+		{
+			value : 1,
+			x : 18,
+			y : 't - 10'
+
+		},
+
+		{
+			value : 1,
+			x : 20,
+			y : 't - 10'
+
+		},
+
+		{
+			value : 5,
+			x : 24,
+			y : 0
+
+
+		},
+
+		{
+			value : 5,
+			x: 50,
+			y: 75
+		},
+
+		{
+			value : 10,
+			x : 36,
+			y : 20
+		},
+
+
+		{
+			value : 10,
+			x : 38,
+			y : 20
+		},
+
+
+		{
+			value : 10,
+			x : 40,
+			y : 20
+		},
+
+		{
+			value : 10,
+			x : 42,
+			y : 18
+		},
+		{
+			value : 10,
+			x : 44,
+			y : 16
+		},
+
+		{
+			value : 10,
+			x : 46,
+			y : 12
+		},
+		{
+			value : 10,
+			x : 48,
+			y : 12
+		},
+		{
+			value : 10,
+			x : 50,
+			y : 12
+		},
+		{
+			value : 10,
+			x : 52,
+			y : 14
+		},
+		{
+			value : 10,
+			x : 54,
+			y : 16
+		},
+		{
+			value : 10,
+			x : 56,
+			y : 14
+		},
+		{
+			value : 10,
+			x : 58,
+			y : 12
+		},
+		{
+			value : 10,
+			x : 60,
+			y : 12
+		},
+		{
+			value : 10,
+			x : 62,
+			y : 14
+		},
+		{
+			value : 10,
+			x : 64,
+			y : 16
+		},
+		{
+			value : 10,
+			x : 66,
+			y : 18
+		},
+		{
+			value : 10,
+			x : 68,
+			y : 20
+		},
+
+		{
+			value : 10,
+			x : 70,
+			y : 20
+		},
+		{
+			value : 10,
+			x : 72,
+			y : 20
+		},
+
+
+		{
+			value: 10,
+			x: 100,
+			y: '10 * sin(t)'
+		},
+		{
+			value: '175-t',
+			x: '(25 * sin(t)) + 150',
+			y: 75
+		},
+
+		{
+			value: 30,
+			x : 90,
+			y : 15
+		}
+	]
+};
+
+let game = GameViz({
+	output: '#svg',
+	stage: STAGE
+});
+
+game.init();
+
+// btn.addEventListener('click', (e) => {
+// 	game.changePlayerFormula('sin(x)');
+// });
+
+game.onEnd((results) => {
+	console.log('save this to firebase:', results);
+});
+
 
 const mathField = MathField()
 document.body.appendChild(mathField.render())
